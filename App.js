ea3372b64113dcf8878fb098c7e00992d36d4451
Matheu5Martins <dev.sincroni@gmail.com>
2021-09-02 20:08:25 -0300

    webview sg_edu

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView source={{ uri: 'https://sgedu.suportegerencial.com.br/login/' }} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
